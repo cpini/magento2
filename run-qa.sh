3004c827518761069a2c85fccb8dbc7b9524fc2d
Cris Pini <cris_pini@yahoo.co.uk>
2020-07-07 17:10:52 +0100

    removed no dev - 18

--- run-qa.sh
+++ run-qa.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 composer install
-#vendor/bin/phpunit -c dev/tests/unit/phpunit.xml.dist
-file="$(ls -l  vendor/bin/phpunit)";
-echo $file;
+vendor/bin/phpunit -c dev/tests/unit/phpunit.xml.dist
