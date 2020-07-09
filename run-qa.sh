8c18d0af87426b95591b5128da5eda85deb0234e
Cris Pini <cris_pini@yahoo.co.uk>
2020-07-09 16:01:52 +0100

    Modified the QA file

--- run-qa.sh
+++ run-qa.sh
@@ -2,7 +2,12 @@
 
 exitCode=0
 
+########################################
+##   ~~~~ install dependencies ~~~~
 composer install
+
+################################################################
+##   ~~~~ CodeSniffer for non core mods ~~~~
 vendor/squizlabs/php_codesniffer/bin/phpcs --standard=vendor/magento/magento-coding-standard/Magento2/ --ignore=/app/code/Magento --extensions=php,phtml app/code
 #vendor/squizlabs/php_codesniffer/bin/phpcs --standard=vendor/magento/magento-coding-standard/Magento2/ --ignore=/app/code/Magento --extensions=php,phtml --severity=error app/code
 status=$?
@@ -15,6 +20,8 @@ else
     exitCode=1
 fi
 
+################################################################
+##   ~~~~ Unit Test for all mods ~~~~
 #vendor/bin/phpunit -c dev/tests/unit/phpunit.xml.dist
 #status=$?
 #if test "$status" -eq 0
