84670cd397d631ceca59e6814beb23ce3434d3a5
Cris Pini <cris_pini@yahoo.co.uk>
2020-07-09 15:57:43 +0100

    Added CS check to QA

--- run-qa.sh
+++ run-qa.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
 
+exitCode=0
+
 composer install
+vendor/squizlabs/php_codesniffer/bin/phpcs --standard=vendor/magento/magento-coding-standard/Magento2/ --ignore=/app/code/Magento --extensions=php,phtml app/code
+#vendor/squizlabs/php_codesniffer/bin/phpcs --standard=vendor/magento/magento-coding-standard/Magento2/ --ignore=/app/code/Magento --extensions=php,phtml --severity=error app/code
+status=$?
+if test "$status" -eq 0
+then
+	# do nothing
+    echo "$status"
+else
+	echo "$status"
+    exitCode=1
+fi
+
 #vendor/bin/phpunit -c dev/tests/unit/phpunit.xml.dist
+#status=$?
+#if test "$status" -eq 0
+#then
+#	echo "SUCCESS"
+#else
+#	echo "$status"
+#fi
+
+exit $exitCode
