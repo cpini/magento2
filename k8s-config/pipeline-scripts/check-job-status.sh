ce818ca0d322460e9e79a76df9541dc643b4a171
Cris Pini <cris_pini@yahoo.co.uk>
2020-10-28 14:44:16 +0000

    Tweaked check job script - 4

--- k8s-config/pipeline-scripts/check-job-status.sh
+++ k8s-config/pipeline-scripts/check-job-status.sh
@@ -10,7 +10,9 @@ done
 
 `gcloud components install kubectl`
 `gcloud container clusters get-credentials $cluster --zone $zone`
+kubeVersion=$(kubectl version)
 echo "gcloud container clusters get-credentials $cluster --zone $zone"
+echo "kubectl version: $kubeVersion"
 while true; do
     status=$(kubectl --request-timeout 2m wait --for=condition=complete job/pre-release-actions)
     response='job.batch/pre-release-actions condition met'
