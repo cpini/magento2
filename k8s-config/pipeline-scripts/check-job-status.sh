64d5c17f4de5b3f5e45317213404e89316010f4d
Cris Pini <cris_pini@yahoo.co.uk>
2020-10-28 14:28:59 +0000

    Tweaked check job script - 3

--- k8s-config/pipeline-scripts/check-job-status.sh
+++ k8s-config/pipeline-scripts/check-job-status.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+while getopts c:z: flag
+do
+    case "${flag}" in
+        c) cluster=${OPTARG};;
+        z) zone=${OPTARG};;
+    esac
+done
+
 `gcloud components install kubectl`
-`gcloud container clusters get-credentials magento-dev-cluster --zone europe-west2-a`
+`gcloud container clusters get-credentials $cluster --zone $zone`
+echo "gcloud container clusters get-credentials $cluster --zone $zone"
 while true; do
-    status=$(kubectl wait --for=condition=complete job/pre-release-actions)
+    status=$(kubectl --request-timeout 2m wait --for=condition=complete job/pre-release-actions)
     response='job.batch/pre-release-actions condition met'
     if test "$status" == "$response"
     then
@@ -17,6 +26,7 @@ done
 ret=$(kubectl delete job pre-release-actions)
 if test "ret" != ""
 then
+    echo "Job deleted: $ret"
     exit 0
 fi  
 exit 1
