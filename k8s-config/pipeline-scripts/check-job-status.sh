06aa6170b8faff27f63defe887ab682250c0d991
Cris Pini <cris_pini@yahoo.co.uk>
2020-10-28 12:15:12 +0000

    Added new step to pipeline

---
+++ k8s-config/pipeline-scripts/check-job-status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+`gcloud components install kubectl`
+`gcloud container clusters get-credentials magento-dev-cluster --zone europe-west2-a`
+while true; do
+    status=$(kubectl wait --for=condition=complete job/pre-release-actions)
+    response='job.batch/pre-release-actions condition met'
+    if test "$status" == "$response"
+    then
+        echo "$status >> $response\n"
+        `kubectl delete job pre-release-actions`
+        break
+    else
+        echo "Still running\n"
+        sleep 10
+    fi
+done
+exit 0
